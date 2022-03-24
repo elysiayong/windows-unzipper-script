c20a134d95564a01d6d943b8afcc1f422891930f
Elysia Yong <77374945+elysiayong@users.noreply.github.com>
2022-03-23 20:44:53 -0400

    Fix where nested zips don't unzip

--- unzipper-unzip-2/mega_unzipper_updated.bat
+++ unzipper-unzip-2/mega_unzipper_updated.bat
@@ -2,21 +2,18 @@
 echo Close this window or press Ctrl+C to exit
 
 :SCAN
-if not exist "%cd%\original_files" mkdir %cd%\original_files
+if not exist "%cd%\original_zips" mkdir %cd%\original_zips
 if not exist "%cd%\put_your_zips_in_here" mkdir %cd%\put_your_zips_in_here
-if not exist "%cd%\extracted_files" mkdir %cd%\extracted_files
 
-set original_files_fp=%cd%\original_files
-set extracted_files_fp=%cd%\extracted_files
+set original_zips_fp=%cd%\original_zips
 
-for /R %%f in (put_your_zips_in_here\*.zip put_your_zips_in_here\*.rar put_your_zips_in_here\*.7z) do (
-    echo %%f | find /v "%cd%\original_files" 1>nul 2>nul && (
+for /R put_your_zips_in_here %%f in (*.zip *.rar *.7z) do (
+    echo %%f | find /v "%cd%\original_zips" 1>nul 2>nul && (
         echo %%f is extracting...
         if not exist "%%~dpf%%~nf" mkdir "%%~dpf%%~nf"
         tar -xf "%%f" -C "%%~dpf%%~nf" 
-        move "%%~dpf%%~nf" "%extracted_files_fp%"
-        move "%%f" "%original_files_fp%"
-        echo Extraction complete!
+        move "%%f" "%original_zips_fp%"
+        echo Extraction complete! Delete the zips located in original_zips!
     ) 
 )
 
